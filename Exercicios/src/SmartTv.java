ce1e5b0db5446c1c709ebe11a1482b57f7271156
maxwell <maxihcpride@hotmail.com>
2023-08-28 16:07:19 -0300

    alteracao

---
+++ Exercicios/src/SmartTv.java
@@ -0,0 +1,37 @@
+public class SmartTv {
+
+    //ATRIBUTOS
+    boolean ligada = false;
+    int canal = 7;
+    int volume = 25;
+    
+    //METODOS
+    public void ligar (){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+
+    }
+
+    public void aumentarVolume(){
+        volume++; 
+    }
+
+    public void diminuirVolume(){
+        volume--;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+}
---
+++ Exercicios/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+     
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+
+        System.out.println(" TV ligada? " + smartTv.ligada);
+        System.out.println(" Canal ? " + smartTv.canal);
+        System.out.println(" Volume? " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println(" TV ligada? " + smartTv.ligada);
+
+        smartTv.mudarCanal(13);
+        System.out.println(" Canal " + smartTv.canal);
+
+        
+
+        
+
+
+
+    }
+   
+}
